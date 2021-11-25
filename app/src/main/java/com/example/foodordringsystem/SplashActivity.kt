493742fa210fae797a7b1aebb014420641b34e2c
abdelrahman <a.ghareb@merge.ws>
2021-11-25 10:41:03 +0200

    init temp splash

---
+++ app/src/main/java/com/example/foodordringsystem/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.foodordringsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    var anim : Animation ?= null
+    var imageView: ImageView ?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        imageView=findViewById(R.id.imageView2)
+        anim = AnimationUtils.loadAnimation(
+            applicationContext,
+            android.R.anim.fade_in
+        ) // Create the animation.
+
+        anim!!.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+            override fun onAnimationEnd(animation: Animation) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                // HomeActivity.class is the activity to go after showing the splash screen.
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {}
+        })
+
+        imageView!!.startAnimation(anim);
+    }
+}
